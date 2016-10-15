eaba918e39ead573eddc83843429b5098040f713
mtorres0612 <mntorres38@yahoo.com.ph>
2016-10-15 19:08:23 +0800

    Base Change

---
+++ FlatPlanet/Controllers/NumbersController.cs
@@ -0,0 +1,52 @@
+﻿using FlatPlanet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FlatPlanet.Controllers
+{
+    public class NumbersController : Controller
+    {
+        NumberContext _context = new NumberContext();
+
+        // GET: Numbers
+        public ActionResult Index(int counter = 0)
+        {
+            @ViewBag.Counter = counter;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Numbers number)
+        {
+            Numbers item = _context.Numbers.OrderByDescending(x => x.Id).Take(1).FirstOrDefault();
+
+            if (item != null)
+            {
+                if (item.Counter >= 10)
+                {
+                    ModelState.AddModelError("", "Counter already reached 10!");
+                    @ViewBag.Counter = item.Counter;
+                    return View();
+                }
+                else
+                {
+                    item.Counter = item.Counter + 1;
+
+                    _context.Numbers.Add(item);
+                    _context.SaveChanges();
+                    @ViewBag.Counter = item.Counter;
+                    return View();
+                }
+            }
+
+            number.Counter = number.Counter + 1;
+
+            _context.Numbers.Add(number);
+            _context.SaveChanges();
+            @ViewBag.Counter = number.Counter;
+            return View();
+        }
+    }
+}
---
+++ FlatPlanet/Models/Numbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace FlatPlanet.Models
+{
+    public class Numbers
+    {
+        public int Id { get; set; }
+        public int Counter { get; set; }
+    }
+
+    public class NumberContext : DbContext
+    {
+        public NumberContext()
+            : base("DefaultConnection")
+        {
+
+        }
+        public DbSet<Numbers> Numbers { get; set; }
+    }
+}
